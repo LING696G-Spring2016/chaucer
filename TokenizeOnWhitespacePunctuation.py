18846d08e25571abf8b9597ec307aac816a1e1da
kyajmiller <zjm203@yahoo.com>
2016-04-17 11:44:43 -0700

    remove references to stopwords

--- TokenizeOnWhitespacePunctuation.py
+++ TokenizeOnWhitespacePunctuation.py
@@ -2,16 +2,13 @@ import re
 
 
 class TokenizeOnWhitespacePunctuation(object):
-    def __init__(self, stringToTokenize, applyStopwords=False, keepCaps=False):
+    def __init__(self, stringToTokenize, keepCaps=False):
         self.keepCaps = keepCaps
-        self.applyStopwords = applyStopwords
 
         if self.keepCaps:
             self.stringToTokenize = stringToTokenize
         else:
             self.stringToTokenize = stringToTokenize.lower()
-
-        self.listOfStopwords = StopwordsList.stopwords()
 
         self.unigrams = []
         self.bigrams = []
@@ -21,9 +18,5 @@ class TokenizeOnWhitespacePunctuation(object):
         self.unigrams = []
         unfileredUnigrams = re.findall(r"[\w']+", self.stringToTokenize)
         for word in unfileredUnigrams:
-            if self.applyStopwords == True:
-                if word not in self.listOfStopwords:
-                    self.unigrams.append(word)
-            else:
-                self.unigrams.append(word)
+            self.unigrams.append(word)
         return self.unigrams
