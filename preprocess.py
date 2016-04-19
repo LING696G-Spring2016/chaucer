26df2190eefefed054442d1e0548af48226734e5
Benjamin <benmullins@email.arizona.edu>
2016-04-18 19:24:51 -0700

    Script to automatically tokenize every file and save the output.

---
+++ preprocess.py
@@ -0,0 +1,24 @@
+import os
+from TokenizeOnWhitespacePunctuation import TokenizeOnWhitespacePunctuation as tkn
+
+
+def preprocess(filename, outdir="/data/tokenized/"):
+    # Open input and output files, creating output file as necessary
+    infile = open(filename, 'r')
+    outfilename = str(os.getcwd()) + str(outdir) + str(filename)
+    os.makedirs(os.path.dirname(outfilename), exist_ok=True)
+    outfile = open(outfilename, 'w')
+
+    for line in infile:
+        tokenizer = tkn(line)
+        tokenized = tokenizer.getUnigrams()
+        print(' '.join(tokenized), file=outfile)
+
+    infile.close()
+    outfile.close()
+    return
+
+
+for dirpath, dirnames, filenames in os.walk('data/raw'):
+    for name in filenames:
+        preprocess(os.path.join(dirpath, name))
